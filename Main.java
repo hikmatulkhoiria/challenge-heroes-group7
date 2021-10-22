126158c9e93e80e3c6d16ba93c0bfaca47834c03
Hikmatul Khoiria <hikmatulkhoiria@gmail.com>
2021-10-22 11:45:30 +0700

    first commit

---
+++ Main.java
@@ -0,0 +1,27 @@
+class Main {
+  public static void main(String[] args) {
+    Heroes hero = new Heroes();
+
+    hero.setProfile("Hikmatul", 20, 164);
+    hero.profile();
+   
+
+    hero.jalan();
+    
+    hero.lari();
+    hero.lari();
+    hero.lari();
+
+    hero.makan();
+
+    hero.minum();
+
+    hero.lompat();
+    hero.lompat();
+
+    hero.duduk();
+    
+    hero.profile();
+    
+  }
+}
